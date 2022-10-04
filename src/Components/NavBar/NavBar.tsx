b6012ccd09edee144767238cc160793e9252dfba
rajkumarverapatnam <rajkumar.verapatnam@gmail.com>
2022-10-04 12:20:03 +0530

    work on the responsive

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,32 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import mobLogo from "../../Assets/Images/mobLogo.jpeg";
 import logo1a from "../../Assets/Images/logo1a.jpeg";
 import logo2a from "../../Assets/Images/logo2a.jpeg";
 import { Link } from "react-router-dom";
 import "./NavBar.css";
+
 export default function NavBar() {
+  const [toggleClass, setToggleClass] = useState(false);
+  const handleNavBar = (): void => {
+    console.log("check");
+    setToggleClass((toggleClass) => !toggleClass);
+    console.log(toggleClass);
+  };
+
   return (
     <div className="nav-menu">
-      <nav className="navBar">
+      <img src={mobLogo} alt="logo1" className="mobLogo" />
+      <nav className={`${toggleClass ? "navBar" : "toggleClass"}`}>
         <div className="nav-left">
           <img src={logo1a} alt="logo1" />
-          <ul>
+          <ul className="nav-items">
             <li>
               Clincal <i className="fa fa-angle-down" aria-hidden="true"></i>
             </li>
             <li>
               <Link to="/GraphDesign">User Graph</Link>
+              <i className="fa fa-angle-down" aria-hidden="true"></i>
             </li>
             {/* <li>
               <Link to="/GraphDesign">Foo</Link>
             </li> */}
             <li>
               <Link to="/TableDesign">User Table</Link>
+              <i className="fa fa-angle-down" aria-hidden="true"></i>
             </li>
           </ul>
         </div>
         <div className="nav-right">
           <img src={logo2a} alt="logo2" />
-          <ul>
+          <ul className="nav-items">
             <li>
               <i className="fa fa-bell" aria-hidden="true"></i>
             </li>
@@ -37,6 +49,7 @@ export default function NavBar() {
           </ul>
         </div>
       </nav>
+      <i className="fa fa-bars" aria-hidden="true" onClick={handleNavBar}></i>
     </div>
   );
 }
